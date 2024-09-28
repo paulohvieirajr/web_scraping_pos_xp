6ac8ebf4f1f36ce29a6226886e5018a13ec82a1b
Paulo Vieira <paulohvieirajr@hotmail.com>
2024-09-28 14:10:05 -0300

    Estrutura inicial do projeto

---
+++ main.py
@@ -0,0 +1,5 @@
+from movimento_falimentar.movimento_falimentar import MovimentoFalimentar
+
+movimento_fa_instance = MovimentoFalimentar()
+
+movimento_fa_instance.execute()
---
+++ movimento_falimentar/movimento_falimentar.py
@@ -0,0 +1,14 @@
+class MovimentoFalimentar():
+
+    def __serach(self) -> bool:
+        result = False
+
+
+
+        return result
+
+
+    def execute() -> bool:
+        result = False
+
+        return result
